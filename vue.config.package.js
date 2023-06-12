f12db4715f68de4f3764682cb330078b09f50ced
liu.shiyi <liu.shiyi@ustcinfo.com>
2023-06-12 10:58:37 +0800

    feat:组件添加移动端适配

--- vue.config.package.js
+++ vue.config.package.js
@@ -82,6 +82,7 @@ module.exports = {
       .add(resolve('packages/Svgs/svg'))
       .add(resolve('packages/assets/images/dataSourceIcon/svg'))
       .add(resolve('packages/assets/images/pageIcon/svg'))
+      .add(resolve('packages/assets/images/terminalIcon/svg'))
       .end()
 
     config.module
@@ -91,6 +92,7 @@ module.exports = {
       .add(resolve('packages/Svgs/svg'))
       .add(resolve('packages/assets/images/dataSourceIcon/svg'))
       .add(resolve('packages/assets/images/pageIcon/svg'))
+      .add(resolve('packages/assets/images/terminalIcon/svg'))
       .end()
       .use('svg-sprite-loader')
       .loader('svg-sprite-loader')
