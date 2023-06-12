a1ab6d11ed9a2ed97cdd2a1fbd5e2ec545607413
liu.shiyi <liu.shiyi@ustcinfo.com>
2023-06-12 11:45:28 +0800

    feat:折线趋势图修改

--- packages/G2Plots/折线图/折线趋势填充图.js
+++ packages/G2Plots/折线图/折线趋势填充图.js
@@ -58,7 +58,7 @@ const setting = [
   /** 样式配置 **/
   // 图表 graph
   {
-    label: '线条宽度',
+    label: '折线宽度',
     type: 'inputNumber',
     field: 'lineStyle_lineWidth',
     optionField: 'lineStyle.lineWidth',
@@ -67,7 +67,7 @@ const setting = [
     groupName: 'graph'
   },
   {
-    label: '颜色配置',
+    label: '折线颜色',
     // 设置组件类型
     type: 'colorSelect',
     // 字段
@@ -79,16 +79,19 @@ const setting = [
     groupName: 'graph'
   },
   {
-    label: '趋势区域透明度',
+    label: '透明度',
     // 设置组件类型
-    type: 'input',
+    type: 'inputNumber',
     // 字段
     field: 'area_style_fillOpacity',
     // 对应options中的字段
     optionField: 'area.style.fillOpacity',
-    value: '0.15',
+    value: 0.15,
     tabName: 'custom',
-    groupName: 'graph'
+    groupName: 'graph',
+    step:0.01,
+    max:1,
+    min:0
   },
   {
     label: '动画效果',
@@ -114,16 +117,17 @@ const setting = [
     groupName: 'graph'
   },
   {
-    label: '动画执行时间',
+    label: '动画时长（ms)',
     // 设置组件类型
-    type: 'input',
+    type: 'inputNumber',
     // 字段
     field: 'animation_appear_duration',
     // 对应options中的字段
     optionField: 'animation.appear.duration',
     value: '5000',
     tabName: 'custom',
-    groupName: 'graph'
+    groupName: 'graph',
+    step:1
   },
   // 网格线 grid
   {
