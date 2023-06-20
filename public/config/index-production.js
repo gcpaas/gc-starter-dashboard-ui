63c48482a04b9aa91b300ea11a41f8c3b88006a9
liu.chengbiao <liu.chengbiao@ustcinfo.com>
2023-06-20 11:55:05 +0800

    仓库迁移

--- public/config/index-production.js
+++
@@ -1,6 +0,0 @@
-window.ENV = 'production'
-var productionConfig = {
-  baseUrl: 'http://gcpaas.gccloud.com/dashboardServer'
-}
-// 必须的
-window.CONFIG = configDeepMerge(window.CONFIG, productionConfig)
